d7337441d8ab2608d970efa7baa9cf3356718446
jjjunyeong <jjjunyeong@gmail.com>
2023-02-26 06:05:40 -0600

    upload summary to flask

--- code/main.py
+++ code/main.py
@@ -4,23 +4,27 @@ from functions.parse_audio import parse_audio, video_to_audio
 from functions.parse_pdf import pdf_to_text, parse_text
 import json
 from summarization import receive_data
-# app = Flask(__name__)
+app = Flask(__name__)
 
 
-# @app.route('/api/v1/machinelearning', methods=['GET', 'POST'])
-# def base():
-#     print('we are at the base!!')
-#     json_data = request.data.decode('utf8')
-#     if request.method == 'POST':
-#         receive_data(json.loads(json_data))
-#         return 'Received a POST request!', 200
+@app.route('/api/v1/machinelearning', methods=['GET', 'POST'])
+def base():
+    print('we are at the base!!')
+    json_data = json.loads(request.data.decode('utf8'))
+    
+    if request.method == 'POST':
+        summary = receive_data(json_data)
+        return 'Received a POST request!', 200
+    
+    elif request.method == 'GET':
+        return summary
 
-# if __name__ == '__main__':
-#     app.run()
+if __name__ == '__main__':
+    app.run()
 
-pdfurl = "https://res.cloudinary.com/dlk3ezbal/image/upload/v1677408827/cramberry/muofg1xcckrqweherqhh.pdf"
-videourl = "https://res.cloudinary.com/dlk3ezbal/video/upload/v1677407694/cramberry/yjtq56cqf9gharq60lfi.mov"
+# pdfurl = "https://res.cloudinary.com/dlk3ezbal/image/upload/v1677408827/cramberry/muofg1xcckrqweherqhh.pdf"
+# videourl = "https://res.cloudinary.com/dlk3ezbal/video/upload/v1677407694/cramberry/yjtq56cqf9gharq60lfi.mov"
 
-data = [{'type': 'pdf', 'url': pdfurl, 'id':0}, {'type':'video', 'url': videourl, 'id':1}]
-summary = receive_data(data)
-print(summary)
+# data = [{'type': 'pdf', 'url': pdfurl, 'id':0}, {'type':'video', 'url': videourl, 'id':1}]
+# summary = receive_data(data)
+# print(summary)
